1b417c424ffcc2f3ef98fa53ddcbd30849fb6472
Al <malrayyan01@gmail.com>
2019-07-13 12:29:39 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          assalaam u alaikum from mma. Today is Staurday July 13th 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
